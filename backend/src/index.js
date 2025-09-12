f1bb33519a40569cc76ed5db9f0f408762f93ea5
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-09-12 10:10:09 +0000

    Chore: Express server configurations

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import { app } from './app.js';
+import dotenv from 'dotenv';
+import { initDatabase } from '/workspaces/Hello_CS618/backend/src/db/init.js';
+dotenv.config();
+
+try {
+    await initDatabase();
+    const PORT = process.env.PORT;
+    app.listen(PORT);
+    console.log(`express is running on http://localhost:${PORT}`);
+} catch (error) {
+    console.error('Error connecting to the database:', error);
+}
