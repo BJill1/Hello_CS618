c2d3c68e768d8d57c55dab045b69e9256f1d3b13
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-09-13 22:26:30 +0000

    Chore: Cors installation

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { postsRoutes } from "/workspaces/Hello_CS618/backend/src/routes/posts.js"
 import bodyParser from "body-parser"
+import cors from "cors"
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 postsRoutes(app)
 app.get('/', (req, res) => {
   res.send('Hello from Express!')
