2e4bb11dbc3c4579b91fb17227b77210df3fcebd
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-09-17 05:35:29 +0000

    Feat: Frontend features

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { App } from '/workspaces/Hello_CS618/src/App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
