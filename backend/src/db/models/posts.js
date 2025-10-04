66fdf46a2a3a432fbd7a819f72215ecb8c907478
BJill1 <157993588+BJill1@users.noreply.github.com>
2025-10-04 15:38:03 +0000

    users added

--- backend/src/db/models/posts.js
+++ backend/src/db/models/posts.js
@@ -3,7 +3,7 @@ const postSchema = new Schema(
   {
     title: { type: String, required: true },
     content: { type: String},
-    author: String,
+    author: { type: Schema.Types.ObjectId, ref: 'user', required: true },
     tags: [String],
   },
   { timestamps: true },
